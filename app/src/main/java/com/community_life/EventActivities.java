35bf61d28ca6eb933a1dfab53c0b9af52d318d82
Amy McNabb <amymcnabb12@gmail.com>
2019-04-08 11:47:10 +0100

    database connected and uploading data

--- app/src/main/java/com/community_life/EventActivities.java
+++ app/src/main/java/com/community_life/EventActivities.java
@@ -3,79 +3,34 @@ package com.community_life;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.ToggleButton;
-import com.community_life.adapters.EventRecyclerAdapter;
-import com.community_life.db_activities.DisplayActivity;
-import com.community_life.db_activities.InsertActivity;
 
 public class EventActivities extends AppCompatActivity
 {
-    RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    RecyclerView.Adapter adapter;
     ToggleButton toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        recyclerView = findViewById(R.id.recycler_view);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        adapter = new EventRecyclerAdapter();
-        recyclerView.setAdapter(adapter);
+        setContentView(R.layout.event_layout);
+        Intent intent = getIntent();
+        intent.getStringExtra("c");
     }
 
     public void onCustomToggleClick(View view)
     {
         toggleButton = findViewById(R.id.toggle);
 
-        Toast.makeText(this, "Favourited", Toast.LENGTH_SHORT).show();
-        toggleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                if(toggleButton.isChecked())
-                {
-                    toggleButton.setBackgroundDrawable(getDrawable(R.drawable.favourited_not));
-                }
-                else
-                {
-                    toggleButton.setBackgroundDrawable(getDrawable(R.drawable.favourited));
-                }
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_card, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        if(((ToggleButton)view).isChecked())
         {
-            return true;
+            Toast.makeText(this, "Added To Favourites", Toast.LENGTH_SHORT).show();
         }
-        return super.onOptionsItemSelected(item);
+        else
+        {
+            Toast.makeText(this, "Removed From Favourites", Toast.LENGTH_SHORT).show();
+        }
     }
 }
